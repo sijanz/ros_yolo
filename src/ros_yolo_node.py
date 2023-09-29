34b719136487d305660814bd58e756ccbb6e71f6
sksijanz <simon.janzon@gmx.net>
2023-09-29 17:17:52 +0200

    added yolo util classes

--- src/ros_yolo_node.py
+++ src/ros_yolo_node.py
@@ -3,10 +3,27 @@
 import rospy
 
 
+YOLO_CONFIDENCE_THRESHOLD = 0.8
+
+
+class YoloSegment:
+    def __init__(self, name, segment_x, segment_y):
+        self.name = name
+        self.segment_x = segment_x
+        self.segment_y = segment_y
+  
+        
+class YoloPose:
+    def __init__(self, x_values, y_values):
+        self.x_values = x_values
+        self.y_values = y_values
+
+
 def ros_yolo():
     
     rospy.init_node('ros_yolo', anonymous=True)
     rate = rospy.Rate(10)
+    print(YOLO_CONFIDENCE_THRESHOLD)
     
     while not rospy.is_shutdown():
         
